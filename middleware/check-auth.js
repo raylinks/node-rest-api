8b49db2b71ecdd45a3bbf4a19d62a42bc9c5feab
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-04-28 22:13:39 +0100

    check-auth middleware

---
+++ middleware/check-auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, CONFIG.jwtSecret);  
+        req.userData = decoded;
+        next(); 
+    } catch (err) {
+        return res.status(401).json({
+            message: 'Not Allowed/ You are not autorized'
+        });
+    }
+     
+};
