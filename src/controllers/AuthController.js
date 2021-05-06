9424b857a03875abcd3a837eee02d25beace3a1d
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-05-06 20:07:03 +0100

    roles and permission

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,6 +3,8 @@ import  {  forbidden } from '../Response/index';
 // GRANT ACCESS TO  PROTECTED ROUTES
 req.user =  currentUser;
 next();
+
+//roles and permission 
 exports.restrictTo =  (...roles) => {
     return (req, res, next) => {
         if(!roles.includes(req.user.role)){
